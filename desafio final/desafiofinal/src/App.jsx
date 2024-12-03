f0a853cc5801ec32a605c7a51fa5e33f1c8ad6aa
Rafael <djrafael16@gmail.com>
2024-12-02 21:43:17 -0300

    Commit inicial desafiofinal

---
+++ desafio final/desafiofinal/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./assets/components/Header/header"
+import Footer from './assets/components/Footer/footer'
+import './global.scss'
+
+export default function App(){
+  return(
+    <>
+    <Header/> 
+    <Footer/> 
+    </>
+  )
+}
